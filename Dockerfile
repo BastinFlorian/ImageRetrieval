599ac606935bb124d16c4ed3470d5b15a3b10aaa
Florian Bastin <florian.bastin@octo.com>
2023-06-23 14:25:23 +0200

    feat(IR): docker compose

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,5 @@ COPY . .
 
 RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
 
-# Streamlit
-# EXPOSE 8501
-# HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
-# ENTRYPOINT ["streamlit", "run", "streamlit.py", "--server.port=8501", "--server.address=0.0.0.0"]
-
-# Uvicorn
-CMD ["uvicorn", "app.app:app", "--host", "0.0.0.0", "--port", "80"]
+EXPOSE 8000:8000
+EXPOSE 8501:8501
--- main_local.py
+++ main_local.py
@@ -6,5 +6,3 @@ if __name__ == '__main__':
     url = "https://c4.staticflickr.com/9/8555/15625756039_a60b0bd0a5_o.jpg"
     model, processor, dataset, dataset_with_embeddings = prepare_model()
     id_results, retrieved_examples, query_image = predict(model, processor, dataset, dataset_with_embeddings, url)
-    id_results, retrieved_examples, query_image = predict(model, processor, dataset, dataset_with_embeddings, url)
-    id_results, retrieved_examples, query_image = predict(model, processor, dataset, dataset_with_embeddings, url)
